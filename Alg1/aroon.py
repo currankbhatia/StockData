dfe7eca1864116ed1b06cb29e83dd8c7f2df0e0b
Curran Bhatia <currankbhatia@gmail.com>
2017-02-10 01:37:02 +0000

    added detection of trending and sp500 feature, but need to work on backend with Yahoo Finance API, not working completely

--- Alg1/aroon.py
+++ Alg1/aroon.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
 import numpy as np
+import csv
 from data_helper import get_historical_data, print_historical_data, plot_data, data_list, plot_data_xy
 from data_helper import data_lists_reversed, print_list, print_lists
 
@@ -48,28 +49,100 @@ def plot_aroon(data):
 
     plt.subplot(212)
     plt.plot_date(dataRev[2], aroonList[0],  '-')
-    #plt.plot_date(dataRev[2], aroonList[1],  '-')
+    plt.plot_date(dataRev[2], aroonList[1],  '-')
     list1 = [50 for i in range(len(dataRev[2]))]
     plt.plot_date(dataRev[2], list1, '-')
     plt.gcf().autofmt_xdate(rotation=45)
     plt.show()
 
-startTime = "2015-01-01"
-endTime = "2016-01-01" 
 
-data = get_historical_data(startTime, endTime, 'TSLA')
+def plot_company(startTime, endTime, company):
+    #Year-Month-Day
+
+    data = get_historical_data(startTime, endTime, company)
+    
+    aroon_data = make_aroon_list(data)
+    
+    #print aroon_data 
+    
+    plot_aroon(data)
+
+    return aroon_data;
+
+def isTrending(numAbove, aroon_data, company):
+    """
+        numAbove: essentially strengh of trend
+        daysPositve: number of days that the aroonIndicator is above numAbove
+    
+        Checks if in reached above the numAbove in 6 out of the last 10 days
+   
+    """
+    daysPositive = 10
+
+    up_data = aroon_data[0]
+    count = 0
+    #for i in range(len(aroon_data)-1, len(aroon_data)-daysPositive, -1):
+    for i in range(len(up_data)-1, len(up_data)-daysPositive-1, -1):
+        #print up_data[i]
+        if up_data[i] >= numAbove:
+           count = count + 1
+            
+    if count < 6: 
+        print company + " : Not trending"
+        return False
+        
+    print company + " : Is Trending"
+    return True
 
 
-dataLists = data_lists_reversed(data)
-dataLs = dataLists[0]
-priceLs = dataLists[1]
-datesLs = dataLists[2] 
+def checkCompanyPositive(startTime, endTime, company, numAbove):
+   
+    print company + " is the company"
+    data = get_historical_data(startTime, endTime, company)
+    
+    aroon_data = make_aroon_list(data)
+    
+    isTrending( numAbove, aroon_data, company)
 
-aroon = make_aroon_list(data)[0]
 
-print_lists([priceLs, aroon, datesLs ])
+def checkSP500Positive(startTime, endTime, numAbove):
+
+    with open('../Summer/constituents.csv') as file:
+        read = csv.DictReader(file)
+        sp500List = list()
+        
+        for row in read: 
+            #print ','.join(row)
+            #print row['Symbol']
+            sp500List.append(row['Symbol'])
+            #print type(row['Symbol'])
+        
+
+        for x in sp500List:
+            checkCompanyPositive(startTime, endTime, x, numAbove)
 
 
-plot_aroon(data)
+#checkSP500Positive("2016-01-01", "2017-02-09",65)
+#checkCompanyPositive("2016-01-01", "2017-02-09", "TSLA", 65)
+
+
+
+plot_company("2016-01-01", "2017-02-09", "TSLA")
+#plot_company("2015-01-01", "2016-01-01", 'TSLA')
+
+
+#startTime = "2015-01-01"
+#endTime = "2016-01-01" 
+#data = get_historical_data(startTime, endTime, 'TSLA')
+#dataLists = data_lists_reversed(data)
+#priceLs = dataLists[1]
+#datesLs = dataLists[2] 
+
+#aroon = make_aroon_list(data)[0]
+
+#print_lists([priceLs, aroon, datesLs ])
+
+
+#plot_aroon(data)
 
 
